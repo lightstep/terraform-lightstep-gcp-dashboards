1d027eb66afb94c7ca10553180ace3f3bf080696
adil.sultan <adil.sultan@lightstep.com>
2022-02-16 15:12:00 -0800

    Cleanup based on tflint checks

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -3,21 +3,3 @@ variable "lightstep_project" {
   type        = string
   default     = "terraform-shop"
 }
-
-variable "lightstep_organization" {
-  description = "Name of Lightstep organization"
-  type        = string
-  default     = "LightStep"
-}
-
-variable "lightstep_env" {
-  description = "Lightstep environment"
-  type        = string
-  default     = "staging"
-}
-
-variable "lightstep_api_key_env_var" {
-  description = "Name of the local environment variable that contains the Lightstep API key"
-  type        = string
-  default     = "LIGHTSTEP_API_KEY"
-}

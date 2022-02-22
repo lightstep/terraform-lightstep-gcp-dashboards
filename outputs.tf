335d4f7b4fc224f1587a96e0b28cbeb0f11108eb
adil.sultan <adil.sultan@lightstep.com>
2022-02-22 13:14:12 -0800

    Fixed outputs.tf to include the additional URLs. Also update ci.yml to add terraform init

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "lightstep_cloud_storage_dashboard_url" {
   value       = module.lightstep_cloud_storage_dashboard.dashboard_url
   description = "Lightstep GCP Cloud Storage Dashboard URL"
 }
+
+output "lightstep_agent_dashboard_url" {
+  value       = module.lightstep_cloud_storage_dashboard.dashboard_url
+  description = "Lightstep GCP Cloud Storage Dashboard URL"
+}
+
+output "lightstep_service_runtime_dashboard_url" {
+  value       = module.lightstep_cloud_storage_dashboard.dashboard_url
+  description = "Lightstep GCP Cloud Storage Dashboard URL"
+}
+
+output "lightstep_logging_metrics_dashboard_url" {
+  value       = module.lightstep_cloud_storage_dashboard.dashboard_url
+  description = "Lightstep GCP Cloud Storage Dashboard URL"
+}

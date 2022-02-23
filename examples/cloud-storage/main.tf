7c99752902c4e81c0aaaf05da0f3ae580a8e30d3
adil.sultan <adil.sultan@lightstep.com>
2022-02-23 10:46:42 -0800

    Fixed examples directory

--- examples/cloud-storage/main.tf
+++ examples/cloud-storage/main.tf
@@ -14,7 +14,7 @@ provider "lightstep" {
   environment     = var.lightstep_env
 }
 
-module "gcp_cloud_storage_dashboard" {
+module "lightstep_cloud_storage_dashboard" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "git::git@github.com:lightstep/terraform-lightstep-aws-dashboards.git//modules/ec2-dashboard?ref=v0.0.1"
